7961520889ed0e79a5269be15c61c75f2386dcb8
Kevin-Baltazar <79995468+Kevin-Baltazar@users.noreply.github.com>
2021-03-03 18:57:59 -0500

    xd2

--- src/Clases/Mantenimiento.java
+++ src/Clases/Mantenimiento.java
@@ -1,51 +1,60 @@
 package Clases;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Mantenimiento {
 
-    private Consumo consumo;
+    private ArrayList<Consumo> consumos = new ArrayList<>();
+    private ArrayList<Servicio> servicios = new ArrayList<>();
     private Computador computador;
     private Persona tecnico;
-    private LocalDateTime fecha;
+    private LocalDate fecha;
 
-    public Mantenimiento(Consumo consumo, Computador computador, Persona tecnico, LocalDateTime fecha) {
-        this.consumo = consumo;
+    public Mantenimiento(Computador computador, Persona tecnico, LocalDate fecha) {
         this.computador = computador;
         this.tecnico = tecnico;
-        this.fecha = LocalDateTime.now();
+        this.fecha = LocalDate.now();
     }
 
-    public Consumo getConsumo() {
-        return consumo;
+    public ArrayList<Consumo> getConsumos() {
+        return consumos;
+    }
+
+    public void setConsumos(ArrayList<Consumo> consumos) {
+        this.consumos = consumos;
+    }
+
+    public ArrayList<Servicio> getServicios() {
+        return servicios;
+    }
+
+    public void setServicios(ArrayList<Servicio> servicios) {
+        this.servicios = servicios;
     }
 
     public Computador getComputador() {
         return computador;
     }
 
-    public Persona getTecnico() {
-        return tecnico;
-    }
-
-    public LocalDateTime getFecha() {
-        return fecha;
-    }
-
-    public void setConsumo(Consumo consumo) {
-        this.consumo = consumo;
-    }
-
     public void setComputador(Computador computador) {
         this.computador = computador;
+    }
+
+    public Persona getTecnico() {
+        return tecnico;
     }
 
     public void setTecnico(Persona tecnico) {
         this.tecnico = tecnico;
     }
 
-    public void setFecha(LocalDateTime fecha) {
-        this.fecha = fecha;
+    public LocalDate getFecha() {
+        return fecha;
     }
 
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
 }
